736b29be73f8fdc48320527fdd6c74be89df5636
PHILIP <philophilo037@gmail.com>
2022-08-20 18:09:53 +0300

    Completed asignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+        alert('I was clicked!');
+      }
+
+      input.addingEventListener('click', clickAlert);
+
 }
+addingEventListener();
